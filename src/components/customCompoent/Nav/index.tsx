3087b64b1a8b0a495d2edc0ca379cd9c0e1aaa69
gilberto19 <gilberto@smartinnovation.com.br>
2024-09-20 21:04:37 -0300

    fix: using theme colors

--- src/components/customCompoent/Nav/index.tsx
+++ src/components/customCompoent/Nav/index.tsx
@@ -1,5 +1,5 @@
 interface NavProps {
-  sections: string[]
+  sections: string[];
 }
 
 export function Nav(props: NavProps) {
@@ -7,11 +7,15 @@ export function Nav(props: NavProps) {
     <nav className="nav hidden lg:block" aria-label="In-page jump links">
       <ul className="mt-16 w-max">
         {props.sections.map((section) => (
-          <li>
-            <a className="group flex items-center py-3 active" href={`#${section}`}>
-              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"></span>
-              {section}
+          <li key={section}>
+            <a
+              className="group flex items-center py-3 active"
+              href={`#${section}`}
+            >
+              <span className="nav-indicator mr-4 h-px w-8 bg-primary transition-all group-hover:w-16 group-hover:bg-primary-light group-focus-visible:w-16 group-focus-visible:bg-primary-light motion-reduce:transition-none"></span>
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-primary-light group-hover:text-primary group-focus-visible:text-primary">
+                {section}
+              </span>
             </a>
           </li>
         ))}
--- src/template/shared/Home/index.tsx
+++ src/template/shared/Home/index.tsx
@@ -1,43 +1,50 @@
+import { AboutMe } from "@/components/customCompoent/AboutMe";
+import { Experience } from "@/components/customCompoent/Experience";
 import { Nav } from "@/components/customCompoent/Nav";
 import { userData } from "@/constants/userData";
 
 const Home = () => {
   return (
-    <div className="flex justify-center min-h-screen max-w-screen px-6 font-sans md:px-12 md:py-24 lg:py-0">
-      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-        <div>
-          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-            <a href="/">{userData.self.completeName}</a>
-          </h1>
-          <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-            {userData.job.position}
-          </h2>
-          <p className="mt-4 max-w-xs leading-normal">
-            {userData.about.resume}
-          </p>
-          <Nav sections={userData.sections} />
-        </div>
-      </header>
-      <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
-        {userData.sections.map((section) =>
-          userData.main[section] ? (
-            <section
-              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-              aria-label={userData.main[section].arialabel}
-              id={section}
-            >
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-                <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
-                  {section}
-                </h2>
-              </div>
-              <div>
-                <p className="mb-4">{userData.main[section].content}</p>
-              </div>
-            </section>
-          ) : null
-        )}
-      </main>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="lg:flex lg:justify-between lg:gap-4">
+        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+          <div>
+            <h1 className="text-4xl font-bold tracking-tight text-onPrimary sm:text-5xl">
+              <a href="/">{userData.self.completeName}</a>
+            </h1>
+            <h2 className="mt-3 text-lg font-medium tracking-tight text-onPrimary sm:text-xl">
+              {userData.job.position}
+            </h2>
+            <p className="mt-4 max-w-xs leading-normal text-onSurface">
+              {userData.about.resume}
+            </p>
+            <Nav sections={userData.sections} />
+          </div>
+          <ul className="ml-1 mt-8 flex items-center">
+            {userData.complement.map((item) => (
+              <li className="mr-5 text-xs shrink-0" key={item.link}>
+                <a
+                  className="block hover:text-primary"
+                  href={item.link}
+                  target="blank"
+                >
+                  <img className="h-6 w-6" src={item.icon} alt="icon" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </header>
+        <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+          <AboutMe
+            title={userData.main.about.title}
+            content={userData.main.about.content}
+          />
+          <Experience
+            title="Experience Work"
+            experiences={userData.experience}
+          />
+        </main>
+      </div>
     </div>
   );
 };
