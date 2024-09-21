8c4d3779628b1cafd6addc61749f6b7f1a786ab7
gilberto19 <gilberto@smartinnovation.com.br>
2024-09-20 22:11:26 -0300

    feat: active nav item by session

--- src/components/customCompoent/Nav/index.tsx
+++ src/components/customCompoent/Nav/index.tsx
@@ -1,20 +1,70 @@
+import { SectionsEnum } from "@/constants/userData";
+import { useEffect, useState } from "react";
+
 interface NavProps {
   sections: string[];
 }
 
-export function Nav(props: NavProps) {
+export function Nav({ sections }: NavProps) {
+  const [activeSection, setActiveSection] = useState("About");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      let currentSection = "";
+      const currSessions = Object.values(SectionsEnum);
+      currSessions.forEach((curr: SectionsEnum) => {
+        const element = document.getElementById(curr);
+        if (element) {
+          const { offsetTop, clientHeight } = element;
+          if (
+            scrollY >= offsetTop - 50 &&
+            scrollY < offsetTop + clientHeight - 50
+          ) {
+            currentSection = `${curr}`;
+          }
+        }
+      });
+
+      setActiveSection(currentSection);
+      console.log(currentSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); // Call once to set the initial active item
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <nav className="nav hidden lg:block" aria-label="In-page jump links">
       <ul className="mt-16 w-max">
-        {props.sections.map((section) => (
-          <li key={section}>
+        {sections.map((curr) => (
+          <li key={curr}>
             <a
-              className="group flex items-center py-3 active"
-              href={`#${section}`}
+              className={`group flex items-center py-3 ${
+                activeSection === curr ? "active" : ""
+              }`}
+              href={`#${curr}`}
             >
-              <span className="nav-indicator mr-4 h-px w-8 bg-primary transition-all group-hover:w-16 group-hover:bg-primary-light group-focus-visible:w-16 group-focus-visible:bg-primary-light motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-primary-light group-hover:text-primary group-focus-visible:text-primary">
-                {section}
+              <span
+                className={`nav-indicator mr-4 h-px w-8 bg-primary transition-all 
+                ${
+                  activeSection === curr
+                    ? "w-16 bg-primary-light"
+                    : "group-hover:w-16 group-hover:bg-primary-light group-focus-visible:w-16 group-focus-visible:bg-primary-light"
+                } 
+                motion-reduce:transition-none`}
+              ></span>
+              <span
+                className={`nav-text text-xs font-bold uppercase tracking-widest 
+                ${
+                  activeSection === curr
+                    ? "text-primary"
+                    : "text-primary-light group-hover:text-primary group-focus-visible:text-primary"
+                }`}
+              >
+                {curr}
               </span>
             </a>
           </li>
